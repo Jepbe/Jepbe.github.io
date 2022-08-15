6e1c6663899c7afb1aee185a4cc98d647ee86190
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-08-15 20:27:10 +0200

    work

--- preimum.js
+++ preimum.js
@@ -1,11 +1,32 @@
 
 
-function initInput() {
-    var variable = "asdodih127faasdiu2261238dsakj";
-    document.forms[0].fid_17.variable = variable;
-    if (document.forms[0].fid_17.variable == variable) {
-        console.log("works")
-    }
-}
+// function initInput() {
+//     var variable = "asdodih127faasdiu2261238dsakj";
+//     document.forms[0].fid_17.variable = variable;
+//     if (document.forms[0].fid_17.variable == variable) {
+//         console.log("works")
+//     }
+//     else {
+//         print("WRONG")
+//     }
+// }
 
-console.log("test")
+console.log("test");
+
+
+
+document.getElementById("sbtn").addEventListener("click", submitbtn);
+
+
+var secretPasscode = "abc69";
+
+function submitbtn() {
+    var mainInput = document.getElementById("minput").value;
+
+    if (mainInput == secretPasscode) {
+        console.log("works")
+    }else {
+        console.log("fuck!")
+    }
+
+}
