efb2b99d1b19221410253ef84051fdc51ac5e345
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-08-15 11:56:40 +0200

    123

---
+++ preimum.js
@@ -0,0 +1,11 @@
+
+
+function initInput() {
+    var variable = "asdodih127faasdiu2261238dsakj";
+    document.forms[0].fid_17.variable = variable;
+    if (document.forms[0].fid_17.variable == variable) {
+        console.log("works")
+    }
+}
+
+console.log("test")
